a4f24ce54d12100aab0aa528d2f9850028a2b635
R4X05Z3R0 <razeroeuw@gmail.com>
2025-11-17 10:46:51 +0100

    Name sorting

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.text.Collator;
+import java.util.Locale;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> names = new ArrayList<>(Arrays.asList(
+                "Zoe", "Asraf", "Mia",
+                "Kevin", "Lina", "Alva",
+                "Ahmed", "Muhammed", "Åge",
+                "Øyvind", "Emil", "Sofie"
+        ));
+
+        Collator danishCollator = Collator.getInstance(Locale.forLanguageTag("da-DK"));
+
+
+        Collections.sort(names);
+
+        for (String name: names){
+            System.out.println(name);
+        };
+    }
+}
